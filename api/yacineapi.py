3c650a05c560b532fcbf50d15cd7ba96e76a54a7
Mohamed Amr Nady <mohamed.amr.nes@gmail.com>
2022-10-06 12:59:11 +0200

    Intial Functioning Release

---
+++ api/yacineapi.py
@@ -0,0 +1,51 @@
+# API by aimadnet
+
+from typing_extensions import Self
+import requests
+import base64
+import time
+import json
+
+
+class YacineTV:
+
+    api_url = "http://ver3.yacinelive.com"
+    key = "c!xZj+N9&G@Ev@vw"
+
+    def __init__(self):
+        pass
+
+    def decrypt(self, enc, key=key):
+        enc = base64.b64decode(enc.encode("ascii")).decode("ascii")
+        result = ""
+        for i in range(len(enc)):
+            result = result + chr(ord(enc[i]) ^ ord(key[i % len(key)]))
+        return result
+
+    def req(self, path):
+        r = requests.get(self.api_url + path, headers={
+                         'User-Agent': 'Safari/537.36'})
+        timestamp = str(int(time.time()))
+        if "t" in r.headers:
+            timestamp = r.headers["t"]
+
+        try:
+            return json.loads(self.decrypt(r.text, key=self.key + timestamp))
+
+        except Exception:
+            return {
+                "success": False,
+                "error": "can't parse json."
+            }
+
+    def get_categories(self):
+        return self.req("/api/categories")
+
+    def get_category(self, category_id):
+        return self.req(f"/api/categories/{str(category_id)}")
+
+    def get_category_channels(self, category_id):
+        return self.req(f"/api/categories/{str(category_id)}/channels")
+
+    def get_channel(self, channel_id):
+        return self.req(f"/api/channel/{str(channel_id)}")
---
+++ cli.py
@@ -0,0 +1,54 @@
+import os
+from api.yacineapi import *
+import pandas as pd
+
+ytv = YacineTV()
+
+print(' ▄▄   ▄▄ ▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄ ▄▄    ▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄   ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄▄ ')
+print('█  █ █  █      █       █   █  █  █ █       █       █  █ █  █  █       █       █   █')
+print('█  █▄█  █  ▄   █       █   █   █▄█ █    ▄▄▄█▄     ▄█  █▄█  █  █   ▄   █    ▄  █   █')
+print('█       █ █▄█  █     ▄▄█   █       █   █▄▄▄  █   █ █       █  █  █▄█  █   █▄█ █   █')
+print('█▄     ▄█      █    █  █   █  ▄    █    ▄▄▄█ █   █ █       █  █       █    ▄▄▄█   █')
+print('  █   █ █  ▄   █    █▄▄█   █ █ █   █   █▄▄▄  █   █  █     █   █   ▄   █   █   █   █')
+print('  █▄▄▄█ █▄█ █▄▄█▄▄▄▄▄▄▄█▄▄▄█▄█  █▄▄█▄▄▄▄▄▄▄█ █▄▄▄█   █▄▄▄█    █▄▄█ █▄▄█▄▄▄█   █▄▄▄█')
+print('')
+print('Script by Nady')
+print('API by aimadnet')
+
+try:
+    categories = pd.DataFrame(ytv.get_categories()['data'])
+except:
+    print('Connection Failed. Try again later')
+    exit()
+print('')
+print('Choose a Category:')
+print('')
+for counter in range(0, categories['id'].count()):
+    print('[' + str(categories.at[counter, 'id'])+'] ' +
+          categories.at[counter, 'name'])
+
+choice = str(input('Enter Your Choice : '))
+category_choice = pd.DataFrame(ytv.get_category_channels(choice)['data'])
+print('')
+print('Choose a Channel:')
+print('')
+for counter in range(0, category_choice['id'].count()):
+    print('[' + str(category_choice.at[counter, 'id'])+'] ' +
+          category_choice.at[counter, 'name'])
+
+choice = str(input('Enter Your Choice : '))
+
+channel_choice = pd.DataFrame(ytv.get_channel(choice)['data'])
+print('')
+print('Choose a Server:')
+print('')
+for counter in range(0, channel_choice['name'].count()):
+    print('[' + str(counter) + '] ' +
+          channel_choice.at[counter, 'name'])
+
+choice = str(input('Enter Your Choice : '))
+
+with open('temp.m3u8', 'wb') as stream:
+    stream.write(channel_choice.at[int(choice), 'url'])
+    stream.close
+os.startfile('temp.m3u8')
